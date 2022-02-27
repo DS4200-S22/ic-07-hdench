a969376ca7d8ceacd5247caf5840cd66d2ca519e
hdench <HenryFDench@gmail.com>
2022-02-27 14:07:12 -0500

    day1

--- js/barchart.js
+++ js/barchart.js
@@ -16,7 +16,7 @@ const margin = {left:50, right:50, bottom:50, top:50};
 const yTooltipOffset = 15; 
 
 
-// TODO: What does this code do? 
+// create svg and with proper viewbox 
 const svg1 = d3
   .select("#hard-coded-bar")
   .append("svg")
@@ -41,32 +41,38 @@ const data1 = [
 
 */ 
 
-// TODO: What does this code do? 
+// find max y value (score)
 let maxY1 = d3.max(data1, function(d) { return d.score; });
 
-// TODO: What does each line of this code do?   
+// Scale function that map data values to pixel values (linear y)
 let yScale1 = d3.scaleLinear()
             .domain([0,maxY1])
             .range([height-margin.bottom,margin.top]); 
 
-// TODO: What does each line of this code do? 
+// Scale function that map data values to pixel values, 
+// in this case using the range of the length of data as base (linear x) 
 let xScale1 = d3.scaleBand()
             .domain(d3.range(data1.length))
             .range([margin.left, width - margin.right])
             .padding(0.1); 
 
 // TODO: What does each line of this code do?  
-svg1.append("g")
+svg1.append("g") // add a placeholder svg inside svg1
    .attr("transform", `translate(${margin.left}, 0)`) 
-   .call(d3.axisLeft(yScale1)) 
-   .attr("font-size", '20px'); 
+    // ^ moves axis to left margin of svg
+   .call(d3.axisLeft(yScale1))  // calls built in d3 function that
+                                // builds y axis off scale
+   .attr("font-size", '20px');  // set font size
 
 // TODO: What does each line of this code do? 
-svg1.append("g")
+svg1.append("g") // add a placeholder svg inside svg1
     .attr("transform", `translate(0,${height - margin.bottom})`) 
-    .call(d3.axisBottom(xScale1) 
+    // ^ moves axis to bottom of svg
+    .call(d3.axisBottom(xScale1)    // calls built in d3 func that builds
+                                    // x axis off of scale
             .tickFormat(i => data1[i].name))  
-    .attr("font-size", '20px'); 
+            // ^ sets the name of the tick on axis? since its a string
+    .attr("font-size", '20px'); // set font size
 
 /* 
 
@@ -75,27 +81,27 @@ svg1.append("g")
 */
 
 // TODO: What does each line of this code do? 
-const tooltip1 = d3.select("#hard-coded-bar") 
-                .append("div") 
-                .attr('id', "tooltip1") 
-                .style("opacity", 0) 
-                .attr("class", "tooltip"); 
+const tooltip1 = d3.select("#hard-coded-bar") // selects all svgs with id
+                .append("div") // preps for adding to div
+                .attr('id', "tooltip1") // adds id to svg called tooltip1
+                .style("opacity", 0)  // sets style to opacity = 0
+                .attr("class", "tooltip"); // sets clas to svg called tooltip
 
 // TODO: What does each line of this code do?  
-const mouseover1 = function(event, d) {
-  tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") 
-          .style("opacity", 1);  
+const mouseover1 = function(event, d) { // creates a function based off of event and data (mouseover)
+  tooltip1.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") // adds text to tooltip1
+          .style("opacity", 1);  // sets opacity = 1 (can be seen)
 }
 
 // TODO: What does each line of this code do? 
-const mousemove1 = function(event, d) {
-  tooltip1.style("left", (event.x)+"px") 
-          .style("top", (event.y + yTooltipOffset) +"px"); 
+const mousemove1 = function(event, d) { // creates a function based off of event and data (mouse moving)
+  tooltip1.style("left", (event.x)+"px") //  UNSURE
+          .style("top", (event.y + yTooltipOffset) +"px"); // UNSURE
 }
 
 // TODO: What does this code do? 
-const mouseleave1 = function(event, d) { 
-  tooltip1.style("opacity", 0); 
+const mouseleave1 = function(event, d) { // creates a function based off of event and data (mouse leaving)
+  tooltip1.style("opacity", 0); // set opacity of tooltip back to 0 (cant be seen)
 }
 
 /* 
@@ -105,23 +111,99 @@ const mouseleave1 = function(event, d) {
 */
 
 // TODO: What does each line of this code do? 
-svg1.selectAll(".bar") 
-   .data(data1) 
-   .enter()  
-   .append("rect") 
-     .attr("class", "bar") 
-     .attr("x", (d,i) => xScale1(i)) 
-     .attr("y", (d) => yScale1(d.score)) 
-     .attr("height", (d) => (height - margin.bottom) - yScale1(d.score)) 
-     .attr("width", xScale1.bandwidth()) 
-     .on("mouseover", mouseover1) 
-     .on("mousemove", mousemove1)
-     .on("mouseleave", mouseleave1);
 
+svg1.selectAll(".bar") // selects the bars
+   .data(data1) // assigns the const data to the bars
+   .enter()  // creates placeholder element for all of the data that arent yet associated with an element
+   .append("rect") // add rect at each placeholder
+     .attr("class", "bar") // add class = bar to these attributes
+     .attr("x", (d,i) => xScale1(i)) // assign horizontal position of bar by scaling
+     .attr("y", (d) => yScale1(d.score)) // assign vertical postiion of bar by scaling
+     .attr("height", (d) => (height - margin.bottom) - yScale1(d.score)) // assign height of bar using score and scaling
+     .attr("width", xScale1.bandwidth()) // assign width of bar using equal scaler
+     .on("mouseover", mouseover1) // calls funct when event happens to the bar
+     .on("mousemove", mousemove1) // calls funct when event happens to the bar
+     .on("mouseleave", mouseleave1); // calls funct when event happens to the bar
 
+// create new svg and with proper viewbox 
+const svg2 = d3
+  .select("#csv-bar")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]); 
 
+  d3.csv("data/barchart.csv").then((data) => {
 
+  // find max y value (score)
+  let maxY2 = d3.max(data, function(d) { return d.score; });
 
+  // Scale function that map data values to pixel values (linear y)
+  let yScale2 = d3.scaleLinear()
+              .domain([0,maxY2])
+              .range([height-margin.bottom,margin.top]); 
 
+  // Scale function that map data values to pixel values, 
+  // in this case using the range of the length of data as base (linear x) 
+  let xScale2 = d3.scaleBand()
+              .domain(d3.range(data.length))
+              .range([margin.left, width - margin.right])
+              .padding(0.1); 
 
+  // TODO: What does each line of this code do?  
+  svg2.append("g") // add a placeholder svg inside svg1
+    .attr("transform", `translate(${margin.left}, 0)`) 
+      // ^ moves axis to left margin of svg
+    .call(d3.axisLeft(yScale2))  // calls built in d3 function that
+                                  // builds y axis off scale
+    .attr("font-size", '20px');  // set font size
 
+  // TODO: What does each line of this code do? 
+  svg2.append("g") // add a placeholder svg inside svg1
+      .attr("transform", `translate(0,${height - margin.bottom})`) 
+      // ^ moves axis to bottom of svg
+      .call(d3.axisBottom(xScale2)    // calls built in d3 func that builds
+                                      // x axis off of scale
+              .tickFormat(i => data[i].name))  
+              // ^ sets the name of the tick on axis? since its a string
+      .attr("font-size", '20px'); // set font size
+
+  // create second tooltop
+  const tooltip2 = d3.select("#csv-bar") // selects all svgs with id
+                  .append("div") // preps for adding to div
+                  .attr('id', "tooltip2") // adds id to svg called tooltip1
+                  .style("opacity", 0)  // sets style to opacity = 0
+                  .attr("class", "tooltip"); // sets class to svg called tooltip
+
+  // SECOND EVENT WATCHERS 
+  const mouseover2 = function(event, d) { // creates a function based off of event and data (mouseover)
+    tooltip2.html("Name: " + d.name + "<br> Score: " + d.score + "<br>") // adds text to tooltip1
+            .style("opacity", 1);  // sets opacity = 1 (can be seen)
+  }
+
+  // TODO: What does each line of this code do? 
+  const mousemove2 = function(event, d) { // creates a function based off of event and data (mouse moving)
+    tooltip2.style("left", (event.x)+"px") //  UNSURE
+            .style("top", (event.y + yTooltipOffset) +"px"); // UNSURE
+  }
+
+  // TODO: What does this code do? 
+  const mouseleave2 = function(event, d) { // creates a function based off of event and data (mouse leaving)
+    tooltip2.style("opacity", 0); // set opacity of tooltip back to 0 (cant be seen)
+  }
+
+  console.log(data);
+  svg2.selectAll(".bar") // Select all bars in SVG
+  .data(data)        
+  .enter()            
+    .append("rect") // add rect at each placeholder
+    .attr("class", "bar") // add class = bar to these attributes
+    .attr("x", (d,i) => xScale2(i)) // assign horizontal position of bar by scaling
+    .attr("y", (d) => yScale2(d.score)) // assign vertical postiion of bar by scaling
+    .attr("height", (d) => (height - margin.bottom) - yScale2(d.score)) // assign height of bar using score and scaling
+    .attr("width", xScale1.bandwidth()) // assign width of bar using equal scaler
+    .on("mouseover", mouseover2) // calls funct when event happens to the bar
+    .on("mousemove", mousemove2) // calls funct when event happens to the bar
+    .on("mouseleave", mouseleave2); // calls funct when event happens to the bar
+
+})
--- js/scatterplot.js
+++ js/scatterplot.js
@@ -8,8 +8,86 @@ Modified: 12/08/21
 
 // Build your scatterplot in this file 
 
+// create new svg and with proper viewbox 
+const svg3 = d3
+  .select("#csv-scatter")
+  .append("svg")
+  .attr("width", width-margin.left-margin.right)
+  .attr("height", height - margin.top - margin.bottom)
+  .attr("viewBox", [0, 0, width, height]); 
 
+  d3.csv("data/scatterplot.csv").then((data3) => {
 
+    let maxX3 = d3.max(data3, (d) => { return d.x; });
+    console.log("Max x: ", + maxX3)
+    
+    let maxY3 = d3.max(data3, (d) => { return d.y; });
+    console.log("Max y: ", + maxY3)
+    
+    let xScale = d3.scaleLinear()
+                .domain([0, maxX3])
+                .range([margin.left, width - margin.right])
+    
+    let yScale = d3.scaleLinear()
+                .domain([0, maxY3])
+                .range([height - margin.bottom, margin.top])
+
+  // TODO: What does each line of this code do?  
+  svg3.append("g") // add a placeholder svg inside svg1
+    .attr("transform", `translate(${margin.left}, 0)`) 
+      // ^ moves axis to left margin of svg
+    .call(d3.axisLeft(yScale3))  // calls built in d3 function that
+                                  // builds y axis off scale
+    .attr("font-size", '20px');  // set font size
+
+  // TODO: What does each line of this code do? 
+  svg3.append("g") // add a placeholder svg inside svg1
+      .attr("transform", `translate(0,${height - margin.bottom})`) 
+      // ^ moves axis to bottom of svg
+      .call(d3.axisBottom(xScale3)    // calls built in d3 func that builds
+                                      // x axis off of scale
+              .tickFormat(i => data3[i].name))  
+              // ^ sets the name of the tick on axis? since its a string
+      .attr("font-size", '20px'); // set font size
+
+  // create second tooltop
+  const tooltip3 = d3.select("#csv-scatter") // selects all svgs with id
+                  .append("div") // preps for adding to div
+                  .attr('id', "tooltip3") // adds id to svg called tooltip1
+                  .style("opacity", 0)  // sets style to opacity = 0
+                  .attr("class", "tooltip"); // sets class to svg called tooltip
+
+  // SECOND EVENT WATCHERS 
+  const mouseover3 = function(event, d) { // creates a function based off of event and data (mouseover)
+    tooltip3.html("Name: " + d.name + "<br> Day: " + d.score + "<br>") // adds text to tooltip1
+            .style("opacity", 1);  // sets opacity = 1 (can be seen)
+  }
+
+  // TODO: What does each line of this code do? 
+  const mousemove3 = function(event, d) { // creates a function based off of event and data (mouse moving)
+    tooltip3.style("left", (event.x)+"px") //  UNSURE
+            .style("top", (event.y + yTooltipOffset) +"px"); // UNSURE
+  }
+
+  // TODO: What does this code do? 
+  const mouseleave3 = function(event, d) { // creates a function based off of event and data (mouse leaving)
+    tooltip3.style("opacity", 0); // set opacity of tooltip back to 0 (cant be seen)
+  }
+
+  console.log(data);
+  svg3.selectAll("circle") // Select all bars in SVG
+  .data(data)        
+  .enter()            
+    .append("circle")
+    .attr("cx", (d) => { return d.x; })
+    .attr("cy", (d) => { return d.y; })
+    .attr("r", 30)
+    .attr("fill", (d) => { return d.color; })
+    .on("mouseover", mouseover3) // calls funct when event happens to the bar
+    .on("mousemove", mousemove3) // calls funct when event happens to the bar
+    .on("mouseleave", mouseleave3); // calls funct when event happens to the bar
+
+})
 
 
 
